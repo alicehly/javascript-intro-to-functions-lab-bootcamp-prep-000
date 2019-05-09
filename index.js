1cb5bed7760df0d643ad1ed5cf70364fa387d684
Alice Ly Ms <alicehly@gmail.com>
2019-05-09 20:01:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// function shout(string) {
-//   return string.toUpperCase()
-// }
+function shout(string) {
+  return string.toUpperCase()
+}
 
-// function whisper(string) {
-//   return string.toLowerCase()
-// }
+function whisper(string) {
+  return string.toLowerCase()
+}
 
-
+function logShout(string) {
+  console.log().toUpperCase()
+}
