28feb340bfaf93a6d6275e8b143926bb18448364
Alice Ly Ms <alicehly@gmail.com>
2019-05-09 20:17:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-}
-
-function sayHiToGrandma(string) {
+  
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
   
 }
+
 
